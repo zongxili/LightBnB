8b37cb9f8bdb26da1bf79502277464386eb0cdeb
zongxili <zongxiliwork@gmail.com>
2019-09-18 18:49:48 -0700

    added something

--- 1_queries/user_login.sql
+++ 1_queries/user_login.sql
@@ -1,3 +1,14 @@
 SELECT id, name, email, password
 FROM users
 WHERE  email = 'tristanjacobs@gmail.com';
+
+
+
+CREATE TABLE guest_reviews(
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
