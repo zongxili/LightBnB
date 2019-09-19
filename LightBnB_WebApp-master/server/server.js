fc79a2899c0cc8fe75f654f913997a8902a86aeb
zongxili <zongxiliwork@gmail.com>
2019-09-18 18:02:48 -0700

    afdas

--- LightBnB_WebApp-master/server/server.js
+++ LightBnB_WebApp-master/server/server.js
@@ -35,4 +35,4 @@ app.get("/test", (req, res) => {
 });
 
 const port = process.env.PORT || 3000; 
-app.listen(port, (err) => console.log(err || `listening on port ${port} 😎`));
+app.listen(port, (err) => console.log(err || `listening on port ${port} 😎`));a
